6c0324987f8d6817d7192746f429ee1177a7f99e
Selecia aaa <garrisonroniece@gmail.com>
2023-12-08 06:27:42 -0500

    worked on model files and server js file

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,2 +1,24 @@
+const { Schema, model } = require("mongoose");
+const mongoose = require('mongoose');
 
-
+const reactionSchema = new Schema(
+    {  
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max_length: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            }
+    },
+);
